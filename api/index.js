748000b2d97d7804ad04242e988f982fb6e631aa
NiteshKG <rachnakgupta81@gmail.com>
2024-07-10 08:52:19 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
+mongoose.connect("mongodb://localhost:27017/realestate").then(()=>{
+    console.log("connected to mongodb")
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 
