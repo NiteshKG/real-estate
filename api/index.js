381ed93530722f4c0231291b4c22dffd817940ae
NiteshKG <rachnakgupta81@gmail.com>
2024-07-10 10:12:30 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import userRouter from './routes/user.route.js'
 
 mongoose.connect("mongodb://localhost:27017/realestate").then(()=>{
     console.log("connected to mongodb")
@@ -13,3 +14,5 @@ app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 }
 );
+
+app.use('/api/user',userRouter)
