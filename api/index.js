ff6f52eeda6f3141c8130d86b2437f04a376213b
NiteshKG <rachnakgupta81@gmail.com>
2024-07-11 14:07:53 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 mongoose.connect("mongodb://localhost:27017/realestate").then(()=>{
     console.log("connected to mongodb")
@@ -9,10 +10,11 @@ mongoose.connect("mongodb://localhost:27017/realestate").then(()=>{
 })
 
 const app = express();
-
+app.use(express.json());
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 }
 );
 
 app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
